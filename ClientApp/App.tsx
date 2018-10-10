06e345ed75002da8398b7dc4ab59f7cf4fc377f2
Newton Sheikh <newtonsheikh@hotmail.com>
2018-10-10 11:38:06 +0000

    added react helloworld

--- ClientApp/App.tsx
+++ ClientApp/App.tsx
@@ -1,1 +1,8 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader';
+
+ReactDOM.render(
+    <h1>Hello React!</h1>,
+    document.getElementById('root')
+  );
